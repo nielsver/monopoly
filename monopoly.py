0a2016b1e29608f323f1a06a156184f4b92bc715
nielsver <nielsvervoort14@gmail.com>
2022-09-29 12:34:11 +0200

    hello world

--- monopoly.py
+++ monopoly.py
@@ -1,11 +1,43 @@
-import pygame
-from sys import exit
+import pygame, sys
+from pygame.locals import *
+
+#Set up pygame
+pygame.init()
+
+
 (width, height) = (1000, 500)
-screen = pygame.display.set_mode((width, height))
-pygame.display.flip()
-pygame.display.set_caption("Hello World")
+windowSurface = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Monopoly")
+
+#Set up the colors
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+
+
+#Set up fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+#Draw the white background onto the surface
+windowSurface.fill(WHITE)
+
+
+text = basicFont.render('HELLO WORLD', True, RED)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+#Draw the text onto the surface
+windowSurface.blit(text,textRect)
+
+#Draw the window onto the screen
+pygame.display.update()
+
+
 while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
