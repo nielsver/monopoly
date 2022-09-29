61fb6a1a0f1f119b95b1c0141fabdab5309a2ef3
johan525 <95874895+johan525@users.noreply.github.com>
2022-09-29 12:00:22 +0200

    Create monopoly.py

---
+++ monopoly.py
@@ -0,0 +1,16 @@
+from operator import truediv
+import pygame
+
+WIDTH, HEIGHT = 1000, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Monopoly")
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
