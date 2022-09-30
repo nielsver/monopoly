74f47f27f2450969cfac20c8f6695d6061c194d4
nielsver <nielsvervoort14@gmail.com>
2022-09-30 14:47:27 +0200

    homescreen

--- monopoly.py
+++ monopoly.py
@@ -1,13 +1,22 @@
+from cgitb import grey
+from hashlib import blake2b
+from string import whitespace
+from turtle import width
 import pygame, sys
 from pygame.locals import *
+
 
 #Set up pygame
 pygame.init()
 
 
-(width, height) = (1000, 500)
-windowSurface = pygame.display.set_mode((width, height))
+
+screen = (1400, 800)
+windowSurface = pygame.display.set_mode(screen)
 pygame.display.set_caption("Monopoly")
+
+width = windowSurface.get_width()
+height = windowSurface.get_height()
 
 #Set up the colors
 BLACK = (0,0,0)
@@ -19,25 +28,53 @@ WHITE = (255,255,255)
 
 #Set up fonts
 basicFont = pygame.font.SysFont(None, 48)
+Largefont = pygame.font.SysFont(None, 80)
 
-#Draw the white background onto the surface
-windowSurface.fill(WHITE)
+#background
+windowSurface.fill(BLACK)
+moneybag = pygame.image.load('./moneysign.jpg').convert()
+background = pygame.image.load('./monopoly_men.png').convert()
+windowSurface.blit(moneybag,(800,0))
+windowSurface.blit(background,(0, 0))
 
+text = basicFont.render('Play', True, RED)
+Monopoly = Largefont.render('monopoly',True, RED)
 
-text = basicFont.render('HELLO WORLD', True, RED)
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
-
-#Draw the text onto the surface
-windowSurface.blit(text,textRect)
-
-#Draw the window onto the screen
-pygame.display.update()
 
 
 while True:
+      
     for event in pygame.event.get():
-        if event.type == QUIT:
+          
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+               #checks if a mouse is clicked
+        if event.type == pygame.MOUSEBUTTONDOWN:
+              
+            #if the mouse is clicked on the
+            # button the game is terminated
+            if width/2-50 <= mouse[0] <= width/2+90 and height/2 <= mouse[1] <= height/2+40:
+                windowSurface.fill(BLACK)
+
+
+    # stores the (x,y) coordinates into
+    # the variable as a tuple
+    mouse = pygame.mouse.get_pos()
+      
+    # if mouse is hovered on a button it
+    # changes to lighter shade 
+    if width/2-50 <= mouse[0] <= width/2+90 and height/2 <= mouse[1] <= height/2+40:
+        pygame.draw.rect(windowSurface,GREEN,[width/2-50,height/2,120,40])
+          
+    else:
+        pygame.draw.rect(windowSurface,BLACK,[width/2-50,height/2,120,40])
+              
+
+    # superimposing the text onto our button
+    windowSurface.blit(text , (width/2-20,height/2))
+    # set monopoly
+    windowSurface.blit(Monopoly, (width/2-120,height-200))
+    #Draw the window onto the screen
+    pygame.display.update() 
